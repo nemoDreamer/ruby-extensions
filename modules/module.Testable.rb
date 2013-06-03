8db8173c78c2986ed47fbf5eb8b87a1b9b27ecb5
Blyth, Philip <PBlyth@cpg.org>
2013-06-03 13:35:46 -0400

    Added module: 'Testable'

---
+++ modules/module.Testable.rb
@@ -0,0 +1,75 @@
+if !'string'.methods.include? :color
+	class String
+		def color (*args) self end
+		def bold ()       self end
+	end
+end
+
+
+module Testable
+
+	# --------------------------------------------------
+	# Basic Assertions
+	# --------------------------------------------------
+
+	def assert_is comparison, strict = true
+		if strict
+			self === comparison
+		else
+			self == comparison
+		end
+	end
+
+	def assert_is_true
+		self.assert_is true
+	end
+
+	def assert_is_false
+		!self.assert_is_true
+	end
+
+	def assert_contains pattern
+		if self.is_a? String
+			self.index(pattern) != nil
+		else
+			nil
+		end
+	end
+
+	def assert_contains_not pattern
+		!assert_contains pattern
+	end
+
+
+	# --------------------------------------------------
+	# Pretty Formatting
+	# --------------------------------------------------
+
+	def pretty *args
+		should = (args[0].is_a? Symbol) ? false : args.shift
+		assertion = args.shift
+
+		result = self.method(assertion).call(*args)
+
+		should = should || assertion.to_s.gsub(/[_-]/, ' ')
+		args = args.to_s.gsub(/^\[(.*)\]$/, '\\1')
+
+		self.inspect.rjust(25) + ' ' + (should.ljust(25) + ' ' + (result ? args : args.bold)).color(result ? 'gray' : 'red')
+	end
+
+	# Short-hands
+	# --------------------------------------------------
+
+	def should_be comparison, strict = true
+		pretty 'should be', :assert_is, comparison, strict
+	end
+
+	def should_contain pattern
+		pretty 'should contain', :assert_contains, pattern
+	end
+
+	def should_not_contain pattern
+		pretty 'should not contain', :assert_contains_not, pattern
+	end
+
+end
