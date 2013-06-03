4b8f66c0031c8a6d693ca039c509d9ebdacb4957
Blyth, Philip <PBlyth@cpg.org>
2013-06-03 13:41:48 -0400

    Modified class: 'Monitor' to use new 'Testable'

--- classes/class.Monitor.rb
+++ classes/class.Monitor.rb
@@ -1,4 +1,13 @@
-require 'classes/class.Curl.rb'
+require File.dirname(__FILE__) + '/class.Curl.rb'
+require File.dirname(__FILE__) + '/../modules/module.Testable.rb'
+
+module Response
+	include Testable
+
+	def inspect
+		'Response'
+	end
+end
 
 ##
 # Monitor
@@ -11,7 +20,7 @@ class Monitor
 
 	def initialize url
 		@url = url
-		@response = Curl::simple @url
+		@response = Curl::simple(@url).extend(Response)
 		@results = []
 	end
 
@@ -19,35 +28,38 @@ class Monitor
 	# Tests
 	# -----
 
-	def should_contain what, pattern
-		self.test what, pattern, "Should contain"
+	def should_contain pattern
+		@results.push @response.should_contain pattern
 	end
 
-	def should_not_contain what, pattern
-		self.test what, pattern, "Should not contain", false
+	def should_not_contain pattern
+		@results.push @response.should_not_contain pattern
 	end
+
+	# def should_contain what, pattern
+	# 	self.test what, pattern, "Should contain"
+	# end
+
+	# def should_not_contain what, pattern
+	# 	self.test what, pattern, "Should not contain", false
+	# end
 
 	##
 	# Helpers
 	# -------
 
-	def test what, pattern, type = '', should_be = true
-		result = self.grep pattern
-		color = (result == should_be) ? 'gray' : 'red'
-		@results.push(type.rjust(25) + ' ' + (what.ljust(25).bold + pattern.to_s).color(color))
-	end
+	# def test what, pattern, type = '', should_be = true
+	# 	result = self.grep pattern
+	# 	color = (result == should_be) ? 'gray' : 'red'
+	# 	@results.push(type.rjust(25) + ' ' + (what.ljust(25).bold + pattern.to_s).color(color))
+	# end
 
 	##
 	# @return [Boolean] matched
 	#
-	def grep pattern
-		# avoid RegExp overhead if simple String
-		if pattern.is_a? String
-			!!@response.index(pattern)
-		else
-			!!@response.grep(pattern)
-		end
-	end
+	# def grep pattern
+	# 	@response.index(pattern) != nil
+	# end
 
 	##
 	# Output
